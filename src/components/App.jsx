d0897ee010819f589c4f4383a122fa73bdec47ba
Mrq9rawi <moh.abdullah02@gmail.com>
2022-07-22 00:07:31 -0400

    validation is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 export default function App() {
+	const userNameRef = React.useRef();
+
+	// Focus On Username Input In Window Load
+	window.onload = () => {
+		userNameRef.current.focus();
+	};
+
+	let userNameDivStatus = "";
+	let passwordDivStatus = "";
+	let confirmPasswordDivStatus = "";
 
 	const [formData, setFormData] = React.useState({
 		userName: "",
@@ -8,7 +18,32 @@ export default function App() {
 		confirmPassword: "",
 	});
 
-	console.log(formData);
+	// Username Validation
+	if (8 <= formData.userName.trim().length && formData.userName.trim().length <= 24) {
+		userNameDivStatus = "success";
+	} else if (formData.userName === "") {
+		userNameDivStatus = "";
+	} else {
+		userNameDivStatus = "error";
+	}
+
+	// Password Validation
+	if (formData.password.trim().length >= 6) {
+		passwordDivStatus = "success";
+	} else if (formData.password === "") {
+		passwordDivStatus = "";
+	} else {
+		passwordDivStatus = "error";
+	}
+
+	// Confirm Password Validation
+	if (formData.confirmPassword === formData.password && formData.confirmPassword !== "") {
+		confirmPasswordDivStatus = "success";
+	} else if (formData.confirmPassword === "") {
+		confirmPasswordDivStatus = "";
+	} else {
+		confirmPasswordDivStatus = "error";
+	}
 
 	function handelChange(event) {
 		setFormData((prevData) => {
@@ -17,7 +52,32 @@ export default function App() {
 				[event.target.name]: event.target.value,
 			};
 		});
+	}
 
+	// Submission Validation
+	function handelSubmission(event) {
+		// I stopped the submission just to prevent the website from reloading.
+		event.preventDefault();
+		if (
+			8 <= formData.userName.trim().length &&
+			formData.userName.trim().length <= 24 &&
+			formData.password.trim().length >= 6 &&
+			formData.confirmPassword === formData.password &&
+			formData.confirmPassword !== ""
+		) {
+			setFormData(() => {
+				return {
+					userName: "",
+					password: "",
+					confirmPassword: "",
+				};
+			});
+			userNameRef.current.focus();
+		} else {
+			formData.confirmPassword === ""
+				? (confirmPasswordDivStatus = "error")
+				: (confirmPasswordDivStatus = "");
+		}
 	}
 
 	return (
@@ -27,9 +87,18 @@ export default function App() {
 			</header>
 			<form id="form">
 				{/* UserName  */}
-				<div className="form-control user-name">
+				<div
+					className={`form-control user-name ${
+						userNameDivStatus === "success"
+							? "success"
+							: userNameDivStatus === "error"
+							? "error"
+							: ""
+					}`}
+				>
 					<label htmlFor="username">Username</label>
 					<input
+						ref={userNameRef}
 						type="text"
 						id="username"
 						placeholder="user123"
@@ -42,7 +111,15 @@ export default function App() {
 					<small> Username must be between 8 & 24 characters </small>
 				</div>
 				{/* Password  */}
-				<div className="form-control password">
+				<div
+					className={`form-control password ${
+						passwordDivStatus === "success"
+							? "success"
+							: passwordDivStatus === "error"
+							? "error"
+							: ""
+					}`}
+				>
 					<label htmlFor="password">Password</label>
 					<input
 						type="password"
@@ -57,7 +134,15 @@ export default function App() {
 					<small>Password must be at least 6 characters</small>
 				</div>
 				{/* Confirm Password */}
-				<div className="form-control confirm-password">
+				<div
+					className={`form-control confirm-password ${
+						confirmPasswordDivStatus === "success"
+							? "success"
+							: confirmPasswordDivStatus === "error"
+							? "error"
+							: ""
+					}`}
+				>
 					<label htmlFor="password2">Confirm Password</label>
 					<input
 						type="password"
@@ -72,7 +157,7 @@ export default function App() {
 					<small>Values don't match</small>
 				</div>
 				{/* Submit button  */}
-				<button>Submit</button>
+				<button onClick={handelSubmission}>Submit</button>
 			</form>
 		</main>
 	);
